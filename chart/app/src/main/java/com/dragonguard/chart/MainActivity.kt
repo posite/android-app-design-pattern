58c71d5c2cbc6e4505b0d68da67751cb22003678
posite <su84053@gmail.com>
2023-03-01 17:02:45 +0900

    feat: 인터넷 상태 확인 구현

--- chart/app/src/main/java/com/dragonguard/chart/MainActivity.kt
+++ chart/app/src/main/java/com/dragonguard/chart/MainActivity.kt
@@ -3,66 +3,110 @@ package com.dragonguard.chart
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.RadarChart
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.ColorFormatter
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var chart: BarChart
+    private lateinit var chart: RadarChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         chart = findViewById(R.id.barchart)
-        val values1 = ArrayList<BarEntry>()
+        val values1 = ArrayList<RadarEntry>()
+        val values2 = ArrayList<RadarEntry>()
 
-        for (i in 0 until 10) {
+        for (i in 0 until 3) {
             val value1 = (Math.random() * 10).toFloat()
             val value2 = (Math.random() * 10).toFloat()
-            values1.add(BarEntry(i.toFloat(), value1))
+            values1.add(RadarEntry(value1))
+            values2.add(RadarEntry(value2))
         }
-        val set1 = BarDataSet(values1, "DataSet 1")
+        val set1 = RadarDataSet(values1, "DataSet 1")
+        set1.fillColor = Color.BLACK
+        set1.setDrawFilled(true)
+        val set2 = RadarDataSet(values2, "DataSet 2")
+        set2.setDrawFilled(true)
 
-        val dataSets1: ArrayList<IBarDataSet> = ArrayList()
+        val dataSets1: ArrayList<IRadarDataSet> = ArrayList()
+        val dataSets2: ArrayList<IRadarDataSet> = ArrayList()
         dataSets1.add(set1) // add the data sets
+        dataSets2.add(set2) // add the data sets
 
-
-
-        val data1 = BarData()
-        data1.addDataSet(set1)
+        val data1 = RadarData(dataSets1)
+        data1.addDataSet(set2)
+        data1.setDrawValues(false)
         set1.color = Color.BLACK
         chart.apply {
-            setDrawValueAboveBar(true)
             setTouchEnabled(false) // 차트 터치 막기
-            setPinchZoom(false) // 두손가락으로 줌 설정
-            setDrawGridBackground(false) // 격자구조
             description.isEnabled = false // 그래프 오른쪽 하단에 라벨 표시
             legend.isEnabled = true // 차트 범례 설정(legend object chart)
-            axisRight.isEnabled = false // 오른쪽 Y축을 안보이게 해줌.
-            axisLeft.apply { //왼쪽 축. 즉 Y방향 축을 뜻한다.
-                axisMinimum = 0f // 최소값 0
-                granularity = 10f // 50 단위마다 선을 그리려고 설정.
-                setDrawLabels(true) // 값 적는거 허용 (0, 50, 100)
-                setDrawGridLines(true) //격자 라인 활용
-                setDrawAxisLine(true) // 축 그리기 설정
-                axisLineColor = ContextCompat.getColor(context, R.color.black) // 축 색깔 설정
-                gridColor = ContextCompat.getColor(context, R.color.purple_200) // 축 아닌 격자 색깔 설정
-                textColor = ContextCompat.getColor(context, R.color.black) // 라벨 텍스트 컬러 설정
-                textSize = 13f //라벨 텍스트 크기
-            }
             xAxis.apply {
                 isEnabled = true
                 position = XAxis.XAxisPosition.BOTTOM //X축을 아래에다가 둔다.
-                granularity = 1f // 1 단위만큼 간격 두기
                 setDrawAxisLine(true) // 축 그림
                 setDrawGridLines(false) // 격자
                 textColor = ContextCompat.getColor(context, R.color.black) //라벨 색상
+                valueFormatter = CodeFormatter()
                 textSize = 12f // 텍스트 크기
             }
         }
         chart.data = data1
+
+        val arr1 : MutableList<String> = mutableListOf("선택하세요", "posite")
+        val spinnerAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr1)
+        val spinner = findViewById<Spinner>(R.id.compare_user_spinner1)
+        spinner.adapter = spinnerAdapter
+        spinner.setSelection(1)
+        if(NetworkCheck.checkNetworkState(this)) {
+            Toast.makeText(applicationContext, "인터넷 연결됨!!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "인터넷 연결 안됨!!", Toast.LENGTH_SHORT).show()
+        }
+        
+        val coroutine = CoroutineScope(Dispatchers.Main)
+        coroutine.launch {
+            while(true) {
+                checkNetworkState()
+                delay(10000L)
+            }
+        }
+
+    }
+
+    private fun checkNetworkState() {
+        if(NetworkCheck.checkNetworkState(applicationContext)) {
+            Toast.makeText(applicationContext, "인터넷 연결됨!!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "인터넷 연결 안됨!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    class CodeFormatter() : ValueFormatter() {
+        private val days = listOf("addition average", "deletion average", "language minimum")
+        //        private val days = listOf( "additions", "deletions")
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            return days.getOrNull(value.toInt()) ?: value.toString()
+        }
+        override fun getFormattedValue(value: Float): String {
+            return "" + value.toInt()
+        }
     }
 }
